dd0ad6f1508b08522fee230c4bfdb0f57d862ade
Victor-Okenwa <okenwavictor003@gmail.com>
2026-05-11 20:46:53 +0100

    Integrate @tanstack/react-router into renderer app
    - Added @tanstack/react-router version 1.169.8 as a dependency in package.json.
    - Implemented router setup in main.tsx, replacing the App component with RouterProvider for improved routing management.
    - Updated pnpm-lock.yaml to reflect new dependencies and their resolutions.

---
+++ apps/renderer/src/routes/__root.tsx
@@ -0,0 +1,12 @@
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { Toaster } from 'sonner'
+
+export const Route = createRootRoute({
+    component: () => (
+        <TooltipProvider>
+            <Outlet />
+            <Toaster closeButton={true} duration={5000} position="top-center" richColors />
+        </TooltipProvider>
+    ),
+})
